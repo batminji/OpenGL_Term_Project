edaed488f57cf3a5d714d24c9264a9fc193e91eb
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-12-14 19:56:51 +0900

    버그방지
    뒤집어지던도중 씬바뀌면 유지되는 버그수정

--- CG_TERM_PROJECT/main.cpp
+++ CG_TERM_PROJECT/main.cpp
@@ -573,6 +573,7 @@ GLvoid drawScene() {
         wheel[0].mesh[0].textureFile = "food/wheel/tomato.png";
         LoadOBJ_single("food/wheel/wheel.obj", wheel[1].mesh);
         wheel[1].mesh[0].textureFile = "food/wheel/cabbage.png";
+        wheel[1].move.y += 0.5;
         tomato.size = 50.0f;
         bowl.size = 1.8f;
         bowl.move.y = -1.0;
@@ -909,6 +910,19 @@ GLvoid drawScene() {
         glUniform4f(objColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);*/
     }
         break;
+    case 10:
+    {  glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(glm::lookAt(glm::vec3(0, 6, 2), glm::vec3(0, 0, 0), cameraUp)));
+    for (int c = 0; c < 2; c++) {
+        for (Mesh m : wheel[c].mesh) {
+            wheel[c].mesh[0].Texturing();
+            m.Bind();
+            glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(wheel[c].my_TR()));
+            m.Draw();
+        }
+    }
+  
+    }
+    break;
     }
 
     // UI
@@ -1562,7 +1576,7 @@ void SpecialKeyboard(int key, int x, int y)
     }
     glutPostRedisplay();
 }
-
+float bread_angle = 0;
 void TimerFunction(int value)
 {
     switch (SCENE) {
@@ -1678,7 +1692,7 @@ void TimerFunction(int value)
             effect_channel->stop();
             bread_fry_sound->release();
         }
-        if (time_angle > 440.0f) SCENE = 4,time_angle = 0;
+        if (time_angle > 440.0f) bread_angle = 0,SCENE = 4,time_angle = 0;
     }
           break;
     case 4:{
@@ -1692,6 +1706,8 @@ void TimerFunction(int value)
             game_result[4] = 2; } }
         if (time_angle > 480.0f) {
             SCENE = 5, time_angle = 0;
+            jcnt = 0;
+            bread_angle = 0;
             effect_channel->stop();
             ssystem->createSound("sound/steak_fry_sound.mp3", FMOD_LOOP_NORMAL, 0, &steak_fry_sound);
             ssystem->playSound(steak_fry_sound, 0, false, &effect_channel);
@@ -1729,6 +1745,7 @@ void TimerFunction(int value)
             steak_fry_sound->release();
         }
         if (time_angle > 440.0f) {
+            bread_angle = 0;
             SCENE = 6, time_angle = 0;
             effect_channel->stop();
             ssystem->createSound("sound/washing.mp3", FMOD_LOOP_NORMAL, 0, &washing_sound);
@@ -1751,7 +1768,7 @@ void TimerFunction(int value)
             effect_channel->stop();
             washing_sound->release();
         }
-        if (time_angle > 440.0f) SCENE = 7, time_angle = 0 , bread_angle = atan2(mx, my);
+        if (time_angle > 440.0f) SCENE = 7, time_angle = 0;
     }
           break;
     case 7:
@@ -1837,7 +1854,7 @@ double mx, my;
 double sx, sy;
 double ex, ey;
 bool left_down = 0;
-float bread_angle = 0;
+
 void Mouse(int button, int state, int x, int y)
 {
     mx = ((double)x - WINDOWX / 2.0) / (WINDOWX / 2.0);
